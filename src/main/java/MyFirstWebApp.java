334af779a1acd7e19de9f170bfeb34ad68c435ae
parthmern <parthmern@gmail.com>
2025-10-26 15:57:12 -0400

    first server

---
+++ src/main/java/MyFirstWebApp.java
@@ -0,0 +1,15 @@
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFirstWebApp extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType("text/html");
+        PrintWriter out = servletResponse.getWriter();
+        out.println("<h2>Parth</h2> ");
+    }
+}
